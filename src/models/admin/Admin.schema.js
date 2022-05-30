1bded39783214805168ce8892c20091b10df36e3
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-05-30 13:44:22 +1000

    models and schema created

---
+++ src/models/admin/Admin.schema.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const AdminSchema = new mongoose.Schema(
+  {
+    fName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [20, "First name must be less than 20 characters"],
+    },
+    lName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [20, "First name must be less than 20 characters"],
+    },
+    dob: {
+      type: Date,
+      default: null,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [50, "Email must be less than 50 characters"],
+      unique: true,
+      index: 1,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [15, "phone number must be less than 15 characters"],
+      maxlength: [10, "phone number must atleast 10 characters"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      default: "n/a",
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("admin", AdminSchema);
