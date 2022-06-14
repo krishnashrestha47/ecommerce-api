55ac5c686cfc94261d65b513f47b26feeebe4faf
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-06-14 10:06:48 +1000

    queries created in category models

--- src/models/category/Category.models.js
+++ src/models/category/Category.models.js
@@ -1,1 +1,22 @@
 import CategorySchema from "./Category.schema";
+
+export const insertCategory = (obj) => {
+  return CategorySchema(obj).save();
+};
+
+export const getACategories = (filter) => {
+  return CategorySchema.findOne(filter);
+};
+
+export const getCategories = (filter) => {
+  return CategorySchema.find(filter);
+};
+
+export const deleteCatById = (_id) => {
+  return CategorySchema.findByIdAndDelete(_id);
+};
+
+// @ _id is string, @updateObj must be an object
+export const updateCategoryById = (_id, updateObj) => {
+  return CategorySchema.findByIdAndUpdate(_id, updateObj);
+};
